5a1418c275c58379fa2090d7e150bd412a006f57
Peter Seibel <peter@gigamonkeys.com>
2011-11-20 15:49:44 -0800

    Removing duplicate macros now that they are inherited.

--- xml-macros.lisp
+++ xml-macros.lisp
@@ -3,14 +3,11 @@
 
 (in-package :monkeylib-html)
 
+;;; These work with XML and HTML
+
 (define-xml-macro :? (name &rest attrs)
   `(:progn
     (:noescape (:format "<?~(~a~) ~@{~(~a~)=\"~a\"~^ ~}?>" ,name ,@attrs))
-    (:newline)))
-
-(define-html-macro :? (name &rest attrs)
-  `(:progn
-    (:noescape (:format "<?~a ~@{~(~a~)=\"~a\"~^ ~}?>" ,name ,@attrs))
     (:newline)))
 
 (define-xml-macro :doctype (name type id url)
@@ -21,4 +18,19 @@
 (define-xml-macro :character (name)
   `(:noescape (:format ,(if (numberp name) "&#~d;" "&~(~a~);") ,name)))
 
+(define-xml-macro :comment (&body body)
+  `(:progn
+    (:noescape "<!-- ")
+    (:newline)
+    ,@body
+    (:newline)
+    (:noescape " -->")))
 
+;;; HTML-specific
+
+(define-html-macro :xhtml (&body body)
+  `(:progn
+     (:? "xml" :version 1.0 :encoding "UTF-8")
+     (:doctype "html" "PUBLIC" "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd")
+    ((:html :xmlns "http://www.w3.org/1999/xhtml" :xml\:lang "en" :lang "en")
+     ,@body)))
